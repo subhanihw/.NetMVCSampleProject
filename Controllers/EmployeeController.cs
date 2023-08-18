d86a12b9e61286772987a230a4ecb0f9755e6361
Subhani Shaik <subhanihw29@gmail.com>
2023-08-18 10:23:08 +0530

    Add Files

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SampleMVCApp.Models;
+
+namespace SampleMVCApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private static List<EmployeeViewModel> employees = new List<EmployeeViewModel>();
+        public IActionResult Index()
+        {
+            return View(employees);
+        }
+        public IActionResult Create()
+        {
+            var emp = new EmployeeViewModel();
+            return View(emp);
+        }
+
+        public IActionResult CreateEmployee(EmployeeViewModel employee)
+        {
+            /*return View("Index");*/
+            employees.Add(employee);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Models/EmployeeViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SampleMVCApp.Models
+{
+    public class EmployeeViewModel
+    {
+        public string Name { get; set; }
+        public string PrimarySkill { get; set; }
+        public int Experience { get; set; }
+    }
+}
